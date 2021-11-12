c3c4299318cbabfcba3b3196b8a3b6a262a43fb0
Phanhieu22 <89361715+Phanhieu22@users.noreply.github.com>
2021-11-12 18:38:11 +0700

    fpt_play

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const btnSubNav = document.getElementById("btnSubNav");
+const coating = document.getElementById("coating");
+const subnav = document.getElementById("subNav");
+
+btnSubNav.onclick = function changeContent() {
+  state.coating = true;
+  state.subNav = true;
+  if (state.coating == true) {
+    coating.classList.add("blockCoating");
+    console.log("hieu");
+  }
+  if (state.subNav == true) {
+    subnav.classList.add("active");
+  }
+};
+coating.onclick = function () {
+  coating.classList.remove("blockCoating");
+  subnav.classList.remove("active");
+};
+var state = {
+  coating: false,
+  subNav: false,
+};
+
+console.log(state);
